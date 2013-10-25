6c8226c8d80aa0a97a211e92fea5a15985c70a49
wils0n <wiljm0.0@gmail.com>
2013-10-25 14:52:08 -0500

    operaciones con tipo de datos

---
+++ comparar_tipos_datos.py
@@ -0,0 +1,19 @@
+
+
+print type("cadena")==str
+print type(1)==int
+
+'''
+OBS:
+En python 2.x, una cadena siempre es mayor a un entero o float
+'''
+
+print "hola">3333333
+print "hola">333.333
+
+'''
+float/int = float
+int/float = float
+
+
+'''
