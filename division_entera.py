adee8abb808f19ed3e37879ee966bfcad6b73cbd
wils0n <wiljm0.0@gmail.com>
2014-01-30 16:41:44 -0500

    upload

---
+++ division_entera.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print 7//2
+
+'''
+al usar // (doble slash), te permite obtener la parte entera de la división 
+'''
---
+++ utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'wilson'
+
+print "hola cómo estas?"
+
+print "bien, mañana iré a verte"
+
+'''
+Usa las 2 primeras lineas de este archivo para cuando utilices tildes o caracteres extraños
+Siempre al inicio del archivo, no despues de algo (como por ejemplo despues de __author__='wilson')
+'''
